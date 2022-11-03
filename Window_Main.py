69d2db2ea8ba426501afe06b74760c408c9ca8ec
j-biography <juhjj147@naver.com>
2022-11-04 02:31:37 +0900

    update

--- Window_Main.py
+++ Window_Main.py
@@ -3,6 +3,8 @@
 import sys
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QLabel
+
+                        
                                                      
 if __name__ == "__main__":
     app = QApplication(sys.argv)
