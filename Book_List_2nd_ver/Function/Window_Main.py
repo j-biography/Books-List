d66e70661f53d0127956cc99b95f81858c13c887
Biography J <tjglobal@tjglobal.co.kr>
2022-11-14 21:24:27 +0900

    update

---
+++ Book_List_2nd_ver/Function/Window_Main.py
@@ -0,0 +1,22 @@
+
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtSql import *
+
+from UI.Main_UI import Main_UI
+from Function.Add_Book import Add_Book
+
+
+class Window_Main(QMainWindow, Main_UI): 
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle('Book List')
+ 
+        self.Add_Book_Window = Add_Book()
+        
+        self.pushButton.clicked.connect()
+        self.pushButton_2.clicked.connect(self.Add_Book_Window.show)
+        self.pushButton_3.clicked.connect()
