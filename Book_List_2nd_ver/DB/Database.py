777991868bcc8ae112e0738a8ee54dfbc8956629
Biography J <tjglobal@tjglobal.co.kr>
2022-11-17 21:46:17 +0900

    update

--- Book_List_2nd_ver/DB/Database.py
+++ Book_List_2nd_ver/DB/Database.py
@@ -1,13 +1,36 @@
 
-import sys
-from PySide6.QtSql import QSqlDatabase, QSqlQuery
+
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtSql import *
 
 
-Database = QSqlDatabase.addDatabase("QSQLITE")
-Database.setDatabaseName("Book_Database")
+def createDB():
+   db = QSqlDatabase.addDatabase('QSQLITE')
+   db.setDatabaseName('Book_DB.db')
+	
+   if not db.open():
+      QMessageBox.critical(None, QApplication.tr("Cannot open database"),
+         QMessageBox.Cancel)
+			
+      return False
+		
+   query = QSqlQuery()
+   
+   
+   query.exec("create table Book(Author , Book_Name)")
+   query.exec("insert into Book values(?, ?)")
+   
+   query.addBindValue()
+   query.addBindValue()
+   
+   query.exec()
 
-
-
-if not Database.open():
-    print("Unable to connect to the database")
-    sys.exit(1)
+   return True
+	
+if __name__ == '__main__':
+   import sys
+	
+   app = QApplication(sys.argv)
+   createDB()
